2718121e903c0480c03497c779ab2216c3ff0ec9
Kavindu Githmin <99247901+KavinduGithmin@users.noreply.github.com>
2022-03-26 07:54:35 +0000

    Initial commit

--- tute04.cpp
+++ tute04.cpp
@@ -11,7 +11,26 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
+// #include <iostream>
+
+// long Factorial(int no);
+// long nCr(int n, int r);
+
+// int main() {
+//   int n, r;
+//   std::cout << "Enter a value for n ";
+//   std::cin >> n;
+//   std::cout << "Enter a value for r ";
+//   std::cin >> r;
+//   std::cout << "nCr = ";
+//   std::cout << nCr(n,r);
+//   std::cout << std::endl;
+//   return 0;
+// }
+
+
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
 long nCr(int n, int r);
@@ -26,4 +45,34 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+
+
+long Factorial(int no){
+  int fac = 1;
+  for(int i=no; i>=1; i--){
+    fac = fac * i;
+  }
+  return fac;
+}
+
+
+long nCr(int num, int x){
+  long factN,factD,factR;
+  factN = Factorial(int no);
+
+  factR = 1;
+  for(int i=x; i>=1; i--){
+    factR = factR * i;
+  }
+  
+  int sub;
+  sub = num - x;
+  factD = 1;
+  for(int i=sub; i>=1; i--){
+    factD = factD * i;
+  }
+
+  return factN / (factD * factR);
+  
 }
